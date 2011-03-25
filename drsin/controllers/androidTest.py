8ffbf19edfc60aeb9632ac746a863eedec000a32
Sina Samangooei <ss@moneo.local>
2011-03-25 08:37:33 +0000

    done

--- drsin/controllers/androidTest.py
+++ drsin/controllers/androidTest.py
@@ -1,7 +1,7 @@
 import logging
 
 from pylons import request, response, session, tmpl_context as c
-from pylons.controllers.util import abort, redirect_to
+from pylons.controllers.util import abort, redirect
 
 from drsin.lib.base import BaseController, render
 
@@ -16,11 +16,11 @@ class AndroidtestController(BaseController):
 		if len(andBit) > 0:
 			andBit = andBit[0].strip()
 			if "2.1" in andBit:
-				return redirect_to("http://padkite.com/PadKite-RC7-v1_00-eclair")
+				return redirect("http://padkite.com/PadKite-RC7-v1_00-eclair")
 			elif "2.2" in andBit: # froyo
 				if "r2" in andBit:
-					return redirect_to("http://padkite.com/PadKite-RC7-v1_00-froyo-FRG83D")
+					return redirect("http://padkite.com/PadKite-RC7-v1_00-froyo-FRG83D")
 				else:
-					return redirect_to("http://padkite.com/PadKite-RC7-v1_00-froyo")
+					return redirect("http://padkite.com/PadKite-RC7-v1_00-froyo")
 			
 		return "<html><body><img src=%s/></body></html>"%pageQRCode
